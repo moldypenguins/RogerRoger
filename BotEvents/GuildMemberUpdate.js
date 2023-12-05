b3b28bf461655ce68783d86896a4bfa6bd60d1a5
Craig Roberts <craig.roberts@crdevel.com>
2023-12-05 01:23:49 -0500

    Update GuildMemberUpdate.js

--- BotEvents/GuildMemberUpdate.js
+++ BotEvents/GuildMemberUpdate.js
@@ -9,26 +9,37 @@ export default {
   name: Events.GuildMemberUpdate,
   once: false,
   async execute(client, oldMember, newMember) {
-    console.log(`OLD: ${util.inspect(oldMember, true, 1, true)}`);
-    console.log(`NEW: ${util.inspect(newMember, true, 1, true)}`);
+    //console.log(`OLD: ${util.inspect(, true, 1, true)}`);
+    //console.log(`NEW: ${util.inspect(newMember.roles.cache, true, 1, true)}`);
 
     let _guild = await Guild.findOne({ guild_id: oldMember.guild.id });
 
-    let _reply = "";
-    if(oldMember.name != newMember.name) {
-      _reply = `**${oldMember.name}** was changed to **${newMember.name}**`;
+    let _reply = [];
+    if(oldMember.nickname != newMember.nickname) {
+      _reply.push(`**${oldMember.nickname}** was changed to **${newMember.nickname}**`);
+    }
+    if(oldMember.roles.cache.size !== newMember.roles.cache.size) {
+      if(oldMember.roles.cache.size < newMember.roles.cache.size) {
+        //role added
+        let _role = newMember.roles.cache.find(r => !oldMember.roles.cache.has(r.id));
+        _reply.push(`**${_role.name}** was added to **${newMember.nickname}**`);
+      } else {
+        //role removed
+        let _role = oldMember.roles.cache.find(r => !newMember.roles.cache.has(r.id));
+        _reply.push(`**${_role.name}** was removed from **${newMember.nickname}**`);
+      }
     }
 
     //admin logging
     client.channels.cache.get(_guild.guild_logs).send({embeds: [
       {
         color: 0x77DD77,
-        description: _reply,
+        description: _reply.join("\n"),
         author: {
           name: 'Guild Member Update',
           icon_url: 'https://i.imgur.com/SrvV95U.png'
         },
       }
     ]});
-  },
+  }
 };
